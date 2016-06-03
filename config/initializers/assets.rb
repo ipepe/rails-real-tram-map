f033e64624ff4dd65c0a0f32cb3a4f1b1604bcac
Patryk Ptasiński <patryk.ptasinski@ipepe.pl>
2016-06-03 17:17:43 +0200

    Backbone of Boostrap and Slim templating

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -1,8 +1,6 @@
-# Be sure to restart your server when you modify this file.
-
-# Version of your assets, change this if you want to expire all your assets.
 Rails.application.config.assets.version = '1.0'
+Rails.application.config.assets.digest = true
+Rails.application.config.assets.precompile += %w( .svg .eot .woff .ttf .woff2 .png)
+Rails.application.config.assets.paths << Rails.root.join('app','assets','fonts/bootstrap-sass-official/assets/fonts/bootstrap')
 
-# Precompile additional assets.
-# application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
-# Rails.application.config.assets.precompile += %w( search.js )
+NonStupidDigestAssets.whitelist += [/\.(?i:)(?:svg|eot|woff|ttf|otf)$/]
