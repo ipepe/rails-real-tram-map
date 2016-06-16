d62940fde2a1a8481c7b9cf86db085546f629285
Patryk Ptasiński <patryk.ptasinski@ipepe.pl>
2016-06-16 09:00:35 +0200

    Wrap scheduler

--- config/initializers/scheduler.rb
+++ config/initializers/scheduler.rb
@@ -1,4 +1,6 @@
-Rails.application.scheduler.every '4s' do
-  puts "Scheduler running task"
-  Tram.refresh_data
+unless defined?(Rails::Console) || File.split($0).last == 'rake'
+  Rails.application.scheduler.every '4s' do
+    puts "Scheduler running task"
+    Tram.refresh_data
+  end
 end
