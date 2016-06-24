d426e3ce74b0c2720d47e17404b947d811b343c3
Patryk Ptasiński <patryk.ptasinski@ipepe.pl>
2016-06-24 22:37:42 +0200

    Better paning to user location

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -1,4 +1,4 @@
-Rails.application.config.assets.version = '1.3'
+Rails.application.config.assets.version = '1.4'
 Rails.application.config.assets.digest = true
 Rails.application.config.assets.precompile += %w( .svg .eot .woff .ttf .woff2 .png)
 Rails.application.config.assets.paths << Rails.root.join('app','assets','fonts/bootstrap-sass-official/assets/fonts/bootstrap')
